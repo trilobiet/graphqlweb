a266da8cbbe12788e3039651890d1702c6b3d816
acdhirr <acdhirr@trilobiet.nl>
2020-04-23 10:54:17 +0200

    revert

--- src/main/java/com/trilobiet/graphqlweb/dao/ArticleDao.java
+++ src/main/java/com/trilobiet/graphqlweb/dao/ArticleDao.java
@@ -3,8 +3,8 @@ package com.trilobiet.graphqlweb.dao;
 import java.util.List;
 import java.util.Optional;
 
+import com.trilobiet.graphqlweb.datamodel.Article;
 import com.trilobiet.graphqlweb.datamodel.Category;
-import com.trilobiet.graphqlweb.datamodel.SortableArticle;
 import com.trilobiet.graphqlweb.datamodel.Topic;
 
 /**
@@ -12,15 +12,15 @@ import com.trilobiet.graphqlweb.datamodel.Topic;
  * @author acdhirr
  *
  */
-public interface ArticleDao<T extends SortableArticle> {
+public interface ArticleDao {
 
 	List<Category> listCategories() throws DaoException;
+
+	List<Article> list(Topic topic, String sort) throws DaoException;
+	List<Article> list(Category category, String sort) throws DaoException;
+	List<Article> find(String searchterm, String sort) throws DaoException;
+	List<Article> find(FieldValueQuery q) throws DaoException;
 	
-	List<T> list(Topic topic, String sort) throws DaoException;
-	List<T> list(Category category, String sort) throws DaoException;
-	List<T> find(String searchterm, String sort) throws DaoException;
-	List<T> find(FieldValueQuery q) throws DaoException;
-	
-	Optional<T> get(String id) throws DaoException;
-	Optional<T> getBySlug(String slug) throws DaoException;
+	Optional<Article> get(String id) throws DaoException;
+	Optional<Article> getBySlug(String slug) throws DaoException;
 }
